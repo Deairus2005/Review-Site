c403001c3c9366b1b32f308a2f6f05db863b4d8b
SymoneSays83 <ninaperkins77@gmail.com>
2021-06-15 13:03:19 -0400

    ProductModel first commit

---
+++ ReviewsSite/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Category { get; set; }
+        public string Review { get; set; }
+
+
+    }
+}
