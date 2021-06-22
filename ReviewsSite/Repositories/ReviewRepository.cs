2df72dd9bc2c4674cd12a897cce83f6c8b016e9f
Your Name <luybrosmusic@gmail.com>
2021-06-22 12:01:34 -0700

    fixed all them errs honey

--- ReviewsSite/Repositories/ReviewRepository.cs
+++ ReviewsSite/Repositories/ReviewRepository.cs
@@ -18,6 +18,17 @@ namespace ReviewsSite.Repositories
             
         }
 
+        public void Create(Review obj)
+        {
+            db.Reviews.Add(obj);
+            db.SaveChanges();
+        }
+
+        public void Delete(Review obj)
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerable<Review> GetAll()
         {
             return db.Reviews.ToList();
@@ -26,6 +37,26 @@ namespace ReviewsSite.Repositories
         public Review GetById(int id)
         {
             return db.Reviews.Where(r => r.ProductId == id).FirstOrDefault();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return db.Products.Find(id);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Review> GetReviews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Review obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
