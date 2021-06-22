baa49d60cbf7068b2515ad4381b3c580d5408db2
Pearl-softdev <76012276+Pearl-softdev@users.noreply.github.com>
2021-06-22 13:12:59 -0400

    added create

---
+++ ReviewsSite/Extensions/ISelectList.cs
@@ -0,0 +1,15 @@
+﻿using ReviewsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Extensions
+{
+    public interface ISelectList
+    {
+        IEnumerable<Product> GetProducts();
+        IEnumerable<Review> GetReviews();
+        Product GetProductById(int id);
+    }
+}
