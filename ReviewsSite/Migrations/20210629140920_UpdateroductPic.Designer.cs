1028aabd28feac45be8a3e125cd43a42cacb9b08
joshuabharper7 <84295436+joshuabharper7@users.noreply.github.com>
2021-06-29 11:26:24 -0400

    FixedUploadPic

--- ReviewsSite/Migrations/20210629140920_UpdateroductPic.Designer.cs
+++ ReviewsSite/Migrations/20210629140920_UpdateroductPic.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReviewsSite;
 
 namespace ReviewsSite.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20210629140920_UpdateroductPic")]
+    partial class UpdateroductPic
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
