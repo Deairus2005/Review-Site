6dca61fe7aa526e668109acc482d540fe5c17c04
joshuabharper7 <84295436+joshuabharper7@users.noreply.github.com>
2021-07-02 14:08:59 -0400

    Complete

---
+++ ReviewsSite.Tests/ProductTests.cs
@@ -0,0 +1,50 @@
+﻿using ReviewsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ReviewsSite.Tests
+{
+    public class ProductTests
+    {
+        public class ReviewTests
+        {
+
+            Product sut;
+
+            [Fact]
+            public void ProductConstructor_Sets_Id_On_Product()
+            {
+                sut = new Product(1, "", "", "");
+
+                Assert.Equal(1, sut.Id);
+            }
+
+            [Fact]
+            public void ProductConstructor_Sets_Name_On_Product()
+            {
+                sut = new Product(1, "Test", "", "");
+
+                Assert.Equal("Test", sut.Name);
+            }
+            [Fact]
+            public void ProductConstructor_Sets_Image_On_Product()
+            {
+                sut = new Product(1, "", "Image.jpg", "");
+
+                Assert.Equal("Image.jpg", sut.Image);
+            }
+            [Fact]
+            public void ProductConstructor_Sets_Category_On_Product()
+            {
+                sut = new Product(1, "", "", "CategotyTest");
+
+                Assert.Equal("CategotyTest", sut.Category);
+            }
+
+        }
+    }
+}
